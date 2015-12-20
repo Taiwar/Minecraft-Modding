31476288252a92afe25219a402443ed21632e7a5
Jonas <thenixchecker@gmail.com>
2015-12-20 23:11:27 +0100

    Updated emerald recipe

--- src/main/java/com/taiwar/theoryofeverything/init/Recipes.java
+++ src/main/java/com/taiwar/theoryofeverything/init/Recipes.java
@@ -3,6 +3,8 @@ package com.taiwar.theoryofeverything.init;
 import cpw.mods.fml.common.registry.GameRegistry;
 import net.minecraft.init.Items;
 import net.minecraft.item.ItemStack;
+import net.minecraftforge.oredict.ShapedOreRecipe;
+import net.minecraftforge.oredict.ShapelessOreRecipe;
 
 public class Recipes
 {
@@ -13,5 +15,6 @@ public class Recipes
         GameRegistry.addRecipe(new ItemStack(ModItems.speculo), "ss"," s","ss", 's', new ItemStack(Items.cookie));
         GameRegistry.addRecipe(new ItemStack(ModBlocks.qgoop), "ss", "ss", 's', new ItemStack(ModItems.qdroplet));
         GameRegistry.addRecipe(new ItemStack(ModItems.coffeedrink), "s", "c", 'c', new ItemStack(ModItems.coffeecup), 's', new ItemStack(Items.coal));
+        GameRegistry.addRecipe(new ShapedOreRecipe(new ItemStack(Items.emerald), "ggg", "gdg", "ggg", 'g', "dyeGreen", 'd', new ItemStack(Items.diamond)));
     }
 }
