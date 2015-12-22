2bed29238d21488a643d13a000214dfa00587837
Jonas <thenixchecker@gmail.com>
2015-12-22 21:32:38 +0100

    further work on tile entities

---
+++ src/main/java/com/taiwar/theoryofeverything/client/gui/GuiBase.java
@@ -0,0 +1,7 @@
+package com.taiwar.theoryofeverything.client.gui;
+
+/**
+ * Created by Jonas on 22.12.2015.
+ */
+public class GuiBase {
+}
---
+++ src/main/java/com/taiwar/theoryofeverything/inventory/ContainerTTOE.java
@@ -0,0 +1,7 @@
+package com.taiwar.theoryofeverything.inventory;
+
+/**
+ * Created by Jonas on 22.12.2015.
+ */
+public class ContainerTTOE {
+}
